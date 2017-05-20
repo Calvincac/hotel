22ac8eec71e5291e0c5c77355c0287230af05cd1
Calvin Custodio <calvincac@hotmail.com>
2017-05-20 18:18:56 -0300

    Retirada de codigo repetido

--- main.php
+++ main.php
@@ -5,6 +5,5 @@ require_once "HotelBuilder.php";
 $input = "Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)";
 
 $hotelBuilder = new HotelBuilder($input);
-$hotelBuilder->buildHotels();
 
 print_r($hotelBuilder->buildHotels()->calculateRate());
